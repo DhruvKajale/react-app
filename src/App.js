0a7792ddafe08203dd0fb40e11d1418cf1628f50
DhruvKajale <dhruvakajale@gmail.com>
2021-06-20 18:59:53 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React, {Component} from 'react';
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/row';
+import Col from 'react-bootstrap/col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+
+
+class App extends Component{
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        userInput: "",
+        list: []
+      }
+    }
+    updateInput(value) {
+      this.setState({
+        userInput: value,
+      });
+    }
+
+    addItem(){
+      if (this.state.userInput !== '') {
+        const userInput = {
+          id: Math.random(),
+          value : this.state.userInput
+        };
+
+        const list = [...this.state.list];
+        list.push(userInput);
+
+        this.setState({
+          list,
+          userInput: ""
+        });
+      }
+    }
+
+    deleteItem(key){
+      const list = [...this.state.list];
+
+      const updateList = list.filter(item => item.id !== key);
+      
+      this.setState({
+        list:updateList,
+      });
+    }
+
+render(){
+  return (
+    <Container>
+      <Row className="row">
+        TODO LIST
+      </Row>
+      <hr />
+      <Row className="main">
+        <Col md={{ span: 5, offset: 4 }}>
+          <InputGroup className="mb-3">
+            <FormControl
+              placeholder="add item"
+              size="lg"
+              value = {this.state.userInput}
+              onChange = {item => this.updateInput(item.target.value)}
+              aria-label="add something"
+              aria-describedby="basic-addon2"
+            />
+            <InputGroup.Append>
+            <Button
+            variant="dark"
+            size="lg"
+            onClick = {()=>this.addItem()}
+            >
+              ADD
+            </Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 5, offset: 4}}>
+          <ListGroup>
+            {this.state.list.map(item => {return (
+              <ListGroup.Item varient="dark" action
+              onClick = {() => this.deleteItem(item.id) }>
+                {item.value}
+              </ListGroup.Item>
+            )})}
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+}
+
+export default App;
